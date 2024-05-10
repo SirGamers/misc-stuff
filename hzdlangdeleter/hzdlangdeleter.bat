81838c0cbc8c0fcb54b7377fd194cc821faa2daa
SirGamers <101012860+SirGamers@users.noreply.github.com>
2024-05-10 17:39:23 -0400

    Create hzdlangdeleter.bat

---
+++ hzdlangdeleter/hzdlangdeleter.bat
@@ -0,0 +1,51 @@
+@ECHO OFF
+:: yes this was typed by hand
+echo Horizon Zero Dawn: Complete Edition Language Deleter
+echo By SirGamers
+echo This script will save 15 GBs of space by deleting all of the language files except for English
+echo WARNING: DO NOT CHANGE THE LANGUAGE AFTER THIS SCRIPT, AS IT MAY BREAK YOUR GAME (I DO NOT KNOW FOR SURE BUT IT IS A HIGH CHANCE)
+pause
+del DLC1_Arabic.bin
+del Initial_Arabic.bin
+del Remainder_Arabic.bin
+echo Arabic deleted sucessfully
+del DLC1_French.bin
+del Initial_French.bin
+del Remainder_French.bin
+echo French deleted sucessfully
+del DLC1_German.bin
+del Initial_German.bin
+del Remainder_German.bin
+echo German deleted sucessfully
+del DLC1_Italian.bin
+del Initial_Italian.bin
+del Remainder_Italian.bin
+echo Italian deleted sucessfully
+del DLC1_LATAMPOR.bin
+del Initial_LATAMPOR.bin
+del Remainder_LATAMPOR.bin
+echo LATAMPOR (just going off the file name) deleted sucessfully
+del DLC1_LATAMSP.bin
+del Initial_LATAMSP.bin
+del Remainder_LATAMSP.bin
+echo LATAMSP (just going off the file name) deleted sucessfully
+del DLC1_Polish.bin
+del Initial_Polish.bin
+del Remainder_Polish.bin
+echo Polish deleted sucessfully
+del DLC1_Portugese.bin
+del Initial_Portugese.bin
+del Remainder_Portugese.bin
+echo Portugese deleted sucessfully
+del DLC1_Russian.bin
+del Initial_Russian.bin
+del Remainder_Russian.bin
+echo Russian deleted sucessfully
+del DLC1_Spanish.bin
+del Initial_Spanish.bin
+del Remainder_Spanish.bin
+echo Spanish deleted sucessfully
+del Remainder_Swedish.bin
+echo The script is done!
+echo If the game updates or you verify the game files, you will (most likly) have to run this script again
+pause
